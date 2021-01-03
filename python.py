aae6129298cccb3205ffcc2550528ae3a797f409
patil5980 <76860757+patil5980@users.noreply.github.com>
2021-01-03 10:50:18 +0530

    Create python.py

---
+++ python.py
@@ -0,0 +1,40 @@
+keys = ["SRK","Salman","Amir"]
+names = {
+  "SRH":20,
+  "Salman":40,
+  "Amir":50,
+}
+print(names)
+
+for key_labels in names.keys():
+   print(key_labels)
+
+for key_labels in names.values():
+   print(key_labels)
+
+names['akshay'] = 60
+print(names)
+
+names["Salman"] = 80
+print(names)
+
+***********
+keys = ["SRK","Salman","Amir"]
+names = {
+  "SRH":20,
+  "Salman":40,
+  "Amir":50,
+}
+print(names)
+
+for key_labels in names.keys():
+   print(key_labels)
+
+for key_labels in names.values():
+   print(key_labels)
+
+names['akshay'] = 60
+print(names)
+
+names["Salman"] = 80
+print(names)
