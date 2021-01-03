c95d0943cdd4dae99bd234795339da8766b16b0d
patil5980 <76860757+patil5980@users.noreply.github.com>
2021-01-03 11:11:31 +0530

    Update python.py

--- python.py
+++ python.py
@@ -18,23 +18,20 @@ print(names)
 names["Salman"] = 80
 print(names)
 
-***********
-keys = ["SRK","Salman","Amir"]
-names = {
-  "SRH":20,
-  "Salman":40,
-  "Amir":50,
-}
-print(names)
+for key_label,value_label in names.items():
+  print(key_label+" "+str(value_label))
+~~~~~~~~~~~~~~~~~~~~~~
 
-for key_labels in names.keys():
-   print(key_labels)
-
-for key_labels in names.values():
-   print(key_labels)
-
-names['akshay'] = 60
-print(names)
-
-names["Salman"] = 80
-print(names)
+{'SRH': 20, 'Salman': 40, 'Amir': 50}
+SRH
+Salman
+Amir
+20
+40
+50
+{'SRH': 20, 'Salman': 40, 'Amir': 50, 'akshay': 60}
+{'SRH': 20, 'Salman': 80, 'Amir': 50, 'akshay': 60}
+SRH 20
+Salman 80
+Amir 50
+akshay 60
