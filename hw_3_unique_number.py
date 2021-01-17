ae1f97aa87d10cdb3d0e4e3a129fa70cb815ea66
patil5980 <76860757+patil5980@users.noreply.github.com>
2021-01-17 20:58:26 +0530

    Create hw_3_unique_number.py

---
+++ hw_3_unique_number.py
@@ -0,0 +1,15 @@
+unique_number={}
+numbers = [1,1,2,3,3,3,4,4,5,6]
+for number in numbers:
+  unique_number[number]=""
+
+for number in  unique_number:
+  print(number)
+  
+##############
+#1
+#2
+#3
+#4
+#5
+#6 
