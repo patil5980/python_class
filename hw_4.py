f3415cb7d454f4bceb3298d5554d3fe75d2bdff0
patil5980 <76860757+patil5980@users.noreply.github.com>
2021-01-28 22:52:23 +0530

    Create hw_4.py

---
+++ hw_4.py
@@ -0,0 +1,18 @@
+number_sq = []
+
+number_sq2 = [2, 3, 5, 6]
+
+for numbers in number_sq2:
+
+  sq_number = numbers*numbers
+  print(sq_number)
+  number_sq.append(sq_number)
+
+print(number_sq)
+
+##########
+#4
+#9
+#25
+#36
+#[4, 9, 25, 36]
