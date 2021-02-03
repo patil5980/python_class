71365efcaa18145b315fabacda902d7338f107f7
patil5980 <76860757+patil5980@users.noreply.github.com>
2021-02-03 21:51:52 +0530

    Create hw_5.py

---
+++ hw_5.py
@@ -0,0 +1,13 @@
+list_1 = ["hello","take"]
+list_2 = ["dear","sir"]
+
+for item in list_1:
+  for item1 in list_2:
+    print(item+" "+item1)
+
+#################
+
+#hello dear
+#hello sir
+#take dear
+#take sir
